5f4c15dae182089d2c833a762fe5d9edec78b1a8
Ícaro Sant'Ana <icarosantana6477@gmail.com>
2023-02-06 22:26:42 -0300

    fix: add variable NEXTAUTH_URL to env

--- apps/client/@types/node.d.ts
+++ apps/client/@types/node.d.ts
@@ -3,6 +3,7 @@ declare global {
         interface ProcessEnv {
             GOOGLE_CLIENT_ID: string;
             GOOGLE_CLIENT_SECRET: string;
+            NEXTAUTH_URL: string;
             NEXTAUTH_SECRET: string;
             SERVER_URL: string;
         }

c3cf440881779dd8c3220209b36c531b83af7693
Ícaro Sant'Ana <icarosantana6477@gmail.com>
2023-04-05 23:20:43 -0300

    fix(server): remove exercise from training if deleted

--- apps/server/src/modules/exercises/mutations/DeleteExercise.ts
+++ apps/server/src/modules/exercises/mutations/DeleteExercise.ts
@@ -5,6 +5,7 @@ import {
     GraphQLBoolean
 } from 'graphql';
 import { ExerciseModel } from '../ExerciseModel';
+import { TrainingModel } from '../../trainings/TrainingModel';
 import { mutationWithClientMutationId } from 'graphql-relay';
 
 export const DeleteExercise = mutationWithClientMutationId({
@@ -18,6 +19,12 @@ export const DeleteExercise = mutationWithClientMutationId({
         if (!user) throw new Error('USER_NOT_AUTHENTICATED');
 
         const exercise = await ExerciseModel.findOne({ _id: _id });
+        const trainings = await TrainingModel.find({ exercises: { $elemMatch: { exercise_id: _id } } });
+
+        for (const training of trainings) {
+            const exercises = training.exercises.filter((exercise) => exercise.exercise_id.toString() !== _id.toString());
+            await TrainingModel.updateOne({ _id: training._id }, { exercises });
+        }
 
         if (!exercise) throw new Error('EXERCISE_NOT_FOUND');
         if (exercise.owner.toString() !== user._id.toString()) throw new Error('USER_NOT_AUTHORIZED');
@@ -29,6 +36,6 @@ export const DeleteExercise = mutationWithClientMutationId({
         deleted: {
             type: GraphQLBoolean,
             resolve: (deleted) => deleted,
-        },    
+        },
     }
 })
